97ad168cffdd21c94470a06f07d5cb9d1bf82dca
karimabouseido <kaabouseido@ucdavis.edu>
2026-04-17 17:34:56 -0700

    further progress on wish

--- project2/wish.cpp
+++ project2/wish.cpp
@@ -13,13 +13,185 @@
 
 using namespace std;
 
+struct parsed {
+    vector<string> args;
+    bool redirect = false; //if command has ">" redirection
+    string outFile;
+};
+
 void print_error() {
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message)); 
 }
 
+string wspaceTrim(const string &s) {
+    size_t start = 0; //first character
+    size_t end = s.size(); //1 + last character
+
+    // Find the first non-whitespace character
+    while (start < end && isspace(s[start])) {
+        start++;
+    }
+
+    // Find the last non-whitespace character
+    while (end > start && isspace(s[end - 1])) {
+        end--;
+    }
+
+    return s.substr(start, end - start); //RETURN PORTION OF STRING W/O START and END WHITESPACE
+}
+
+bool builtinCommand(const parsed& pcommand, vector<string>& path) {
+    const string& cmdName = pcommand.args[0]; //command name is first argument
+    if (cmdName == "exit") {
+        if (pcommand.args.size() != 1) { //exit should have no arguments
+            print_error();
+            return true; //return true since command was handled, even though it was an error
+        }
+        exit(0); //exit the shell
+    }
+
+    if (cmdName == "cd") {
+        if (pcommand.args.size() > 1) { //cd should have only 1 argument
+            print_error();
+            return true;
+        }
+        if (chdir(pcommand.args[1].c_str()) != 0) { //change directory
+            print_error(); //if it's not 0, then chdir failed
+        }
+    }
+
+    if (cmdName == "path") {
+        path.clear(); //clear the /bin path, bin commands using executables shouldn't WORK if no path
+        for (size_t i = 1; i < pcommand.args.size(); i++) { //add each new path argument to path vector
+            path.push_back(pcommand.args[i]);
+        }
+        return true;
+    }
+
+    return false;
+
+}
+
+//this function finds the full executable path to the parameter command by searching shell paths.
+string findExecutable(const string& cmdName, const vector<string>& path) {
+    for (const string& directory : path) { //try each path directory
+        //check if file exists in directory
+        string fullPath = directory; //start with directory to put in this temp string;
+        if (!fullPath.empty() && fullPath.back() != '/') { //if fullPath already doesn't have a slash, add it
+            fullPath += '/';
+        }
+        fullPath += cmdName; //append command name to directory to get full path
+        //Check if it is executable with access()
+        if (access(fullPath.c_str(), X_OK) == 0) { //if access returns 0, then file exists and is executable
+            return fullPath; //return the full path to the executable
+        }
+    }
+    return ""; //for if we finish searching all paths and we cannot find anything
+}
+
+void PathCommand(const parsed& pcommand, const vector<string> &path, vector<pid_t>& child_pid) {
+    if (path.empty()) { //if path is empty (like with when you type the path command with no args), we can't execute bin commands
+        print_error();
+        return;
+    }
+
+    string execPath = findExecutable(pcommand.args[0], path); //find the executable path for the command
+    if (execPath.empty()) { //if we can't find an executable for the command, print error and return
+        print_error();
+        return;
+    }
+
+    pid_t pid = fork(); //fork a child process to execute the command (IMPORTANT!)
+    if (pid < 0) { //if fork fails, print error and return
+        print_error();
+        return;
+    }
+
+}
+
+vector<string> splitChunk(const string &s) {
+    vector<string> pieces; //vector to hold split pieces of string
+    istringstream iss(s); //create input string stream from string s
+    string pieceforVec; //temp variable to push into vector
+    while (iss >> pieceforVec) {
+        pieces.push_back(pieceforVec);
+    }
+    return pieces; //return vector of split pieces of string
+}
+
+bool commandParse(const string& chunk, parsed& pcommand) {
+    pcommand = parsed(); //reset pcommand to default values
+    string cmd = wspaceTrim(chunk); //trim leading and trailing whitespace from chunk
+    int redirCount = 0; //number of ">" in command
+    if (cmd.empty()) { 
+        return false; //cannot have empty chunk, syntax error
+    }
+
+    size_t redirectIndex = string::npos; //index of ">" in command, default to npos (not found)
+    for (size_t i = 0; i < cmd.size(); i++) { //count number of ">" in command
+        if (cmd[i] == '>') {
+            redirCount++;
+            if (redirectIndex == string::npos) { //save index of first ">" only
+                redirectIndex = i;
+            }
+        }
+    }
+
+    if (redirCount > 1) {
+        return false; //cannot have more than one ">" in command, syntax error
+    }
+
+    if (redirCount == 1) { //redirection
+        string leftPart = wspaceTrim(cmd.substr(0, redirectIndex)); //portion of command before ">"
+        string rightPart = wspaceTrim(cmd.substr(redirectIndex + 1)); //portion of command after ">"
+        if (leftPart.empty() || rightPart.empty()) {
+            return false; //both sides of ">" must have text, syntax error
+        }
+
+        //redirection always has a command on the left and output file on the right
+
+        //should only be one chunk on the right side of >, if more than one chunk, syntax error
+        vector<string> rightSplitPieces = splitChunk(rightPart); //if whitespace in right, split into pieces
+        if (rightSplitPieces.size() != 1) {
+            return false;
+        }
+
+        pcommand.args = splitChunk(leftPart); //split left part into command and args
+        if (pcommand.args.empty()) {
+            return false; //need at least a command on the left side, syntax error
+        }
+
+        pcommand.redirect = true;
+        pcommand.outFile = rightSplitPieces[0];
+        return true; //parsed redirection command
+    }
+
+    //FOR NON-REDIRECTION COMMANDS, JUST SPLIT INTO ARGUMENTS
+    pcommand.args = splitChunk(cmd); //split entire command into command and args
+    if (pcommand.args.empty()) {
+        return false; //need at least a command, syntax error
+    }
+    return true; //parsed non-redirection command
+}
+
+vector<string> ParallelCommandSplit(const string& line) {
+    vector<string> commands; //start with empty vector
+    string currentSegment; //start with empty string
+    for (char c : line) { //for every character in line, detect "&" in string so chunks between them get pushed
+        if (c == '&') {
+            commands.push_back(currentSegment);
+            currentSegment.clear();
+        } else {
+            currentSegment.push_back(c); //push character in current segment
+        }
+    }
+    commands.push_back(currentSegment); //make sure to push last segment after EOL is reached
+    return commands; //return vector of ALL commands in parallel command line
+}
+
 int main(int argc, char *argv[]) {
-    vector<int> fdVec;
+    vector<int> fdVec; //file descriptor vector
     vector<string> pathTerm; 
     pathTerm.push_back("/bin"); //all the unix commands are in /bin!
 
@@ -43,9 +215,80 @@ int main(int argc, char *argv[]) {
         interactMode = false;
     }
 
-    while (true) {
+    string line; //string to hold each line of input
+    while (true) { //SHELL IS IN WHILE LOOP WHILE RUNNING
+        if (interactMode == true) {
+            cout << "wish> "; //prompt for user input
+            cout.flush(); //force "wish> " to appear before anything else or user input
+        }
 
+        if (!getline(*input, line)) { //hitting EOF means we exit shell in either mode
+            exit(0);
+        }
 
+        //robust to white space, trim all leading and trailing white space
+        string trimmedLine = wspaceTrim(line);
+        if (trimmedLine.empty()) { //if blank line
+            //cout << "empty! \n";
+            continue; //go back to top of while loop in next iteration for next line of input
+        }
+
+        //PARALLEL COMMAND SPLIT AND SYNTAX CHECK
+        vector<string> commandChunks = ParallelCommandSplit(trimmedLine); //split line into parallel command chunks by "&"
+        
+        bool chunksPresent = false; //checks if at least one command exists after ParallelCommandSplit
+        bool syntaxCheck = true; //checks for syntax error
+        for (const string& chunk : commandChunks) {
+            if (wspaceTrim(chunk).empty()) {
+                syntaxCheck = false; //if chunk is empty, then we have a syntax error (bad & usage)
+            } else {
+                chunksPresent = true;
+            }
+        }
+
+        if (!chunksPresent) { //if chunksPresent is false, then we have a syntax error due to bad "&" usage
+            continue;
+        }
+        if (!syntaxCheck) {
+            print_error();
+            continue;
+        }
+
+        //PARSE INPUT LINE, REDIRECTION, and PATH
+        vector<pid_t> childprocesses; //holds child PIDs
+
+        for (const string& chunk : commandChunks) {
+            parsed pcommand; //parsed command
+            bool pcCheck = commandParse(chunk, pcommand);
+            if (!pcCheck) { //if commandParse returns false, we have a syntax error in the command
+                print_error();
+                continue;
+            }
+
+            if (pcommand.redirect) { //built-n commands do not have REDIRECTION signs in syntax
+                const string& cmdName = pcommand.args[0];
+                if (cmdName == "exit" || cmdName == "cd" || cmdName == "path") {
+                    print_error();
+                    continue;
+                }
+            }
+
+            bool builtinCheck = builtinCommand(pcommand, pathTerm); //checks if command is built-in
+            if (builtinCheck) { //execute built in command logic with this condition
+                continue;
+            }
+
+            PathCommand(pcommand, pathTerm, childprocesses); //execute non-built-in command logic with this function
+        }
+
+        for (size_t i = 0; i < childprocesses.size(); i++) { //wait for all child processes to finish
+            int status = 0;
+            while (waitpid(childprocesses[i], &status, 0) < 0) {
+                if (errno != EINTR) { //if failure not interrupted by signal, stop retrying
+                    break;
+                }
+            }
+        }
     }
     /*
     if (input != nullptr && interactMode) {
